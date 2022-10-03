501343ee3710bc6e4674ecbd299431f91abb76c6
Abdullah Sami <abdullahsami201@gmail.com>
2022-10-03 19:50:53 +0500

    commit

---
+++ Chaudary Autostore/test summery/All_TestLogger.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+ECHO Demo Automation Executed Started.
+
+
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+
+
+VSTest.Console.exe  C:\Users\samiiabd\source\repos\1999abdullah\Chaudary-Autostore\Chaudary Autostore\bin\Debug
+
+
+
+PAUSE
