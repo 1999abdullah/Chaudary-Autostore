c65f2f4e2cfe2e0515136bc44c88441e80962ef8
Abdullah Sami <abdullahsami201@gmail.com>
2022-10-04 09:58:11 +0500

    name changed

--- Chaudary_Autostore/test summery/All_TestLogger.bat
+++ Chaudary_Autostore/test summery/All_TestLogger.bat
@@ -6,7 +6,7 @@ ECHO Demo Automation Executed Started.
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 
 
-VSTest.Console.exe  C:\Users\samiiabd\source\repos\1999abdullah\Chaudary-Autostore\Chaudary Autostore\bin\Debug
+VSTest.Console.exe  C:\Users\samiiabd\Source\Repos\1999abdullah\Chaudary-Autostore\Chaudary_Autostore\bin\Debug\Chaudary_Autostore.dll
 
 
 
